b406ab3546f4b30c3a3910e3b602548d0137733a
Roman <100953835+Chezy13@users.noreply.github.com>
2022-11-15 18:02:55 +0300

    up

--- src/components/common/MainWrapper/MainWrapper.tsx
+++ src/components/common/MainWrapper/MainWrapper.tsx
@@ -1,25 +1,66 @@
 import React, { FC, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import MainPage from '../../../Pages/MainPage/MainPage';
+import { setCardsAction } from '../../../store/reducer/selectedCardReducer';
 import { TABS_CONFIG } from '../../../types/configs';
 import { IWithChildren } from '../../../types/types';
+import { IMovieCardProps } from '../MovieCard/MovieCard';
 import Header from './Header/Header';
 import styles from "./MainWrapper.module.scss"
 import Navigation from './Navigation/Navigation';
 
 const MainWrapper: FC<IWithChildren> = ({children}) => {
+
+    const dispatch = useDispatch();
     const [activeTabItem, setActiveTabItem] = useState<number>(TABS_CONFIG[0].id);
+    const [posts, setPosts] = useState<IMovieCardProps[]>([]);
+
+    const {cards} = useSelector((state: any) => state.selectedCard);
+
     const handleSetActiveTabItem = (id: number) => setActiveTabItem(id);
 
-    useEffect(() => {
-        fetch('http://www.omdbapi.com/?s=Jason&page=2&apikey=55394393')
-        .then(response => response.json())
-        .then(json => console.log(json))
-        }, []);
+    // useEffect(() => {
+    //     fetch('http://www.omdbapi.com/?s=Jason&page=2&apikey=55394393')
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+    //     }, []);
+
+        const setReduxPosts = (payload: IMovieCardProps[]) => {
+            dispatch(setCardsAction(payload))
+        }
 
         const getPosts = async () => {
             const { results: posts } = await fetch("http://www.omdbapi.com/?s=Jason&page=2&apikey=55394393")
             .then(response => response.json())
             .catch(e => console.log(e))
+    
+            if (Array.isArray(posts)) {
+                setReduxPosts(posts)
+            }
         }
+
+        const filterPosts = () => {
+            switch (activeTabItem) {
+                case 2:  
+                    setPosts(cards.filter((card: IMovieCardProps) => card.favorite));
+                    return
+    
+                default:
+                    setPosts(cards)
+            }
+        }
+
+        useEffect(() => {
+            getPosts()
+        }, [])
+
+        useEffect(() => {
+            setPosts(cards)
+        }, [cards])
+
+        useEffect(() => {
+            filterPosts()
+        }, [activeTabItem, cards])
 
     return (
         <div className={styles.wrapper}>
@@ -33,7 +74,7 @@ const MainWrapper: FC<IWithChildren> = ({children}) => {
                     />
                 </nav>
                 <main>
-                    {children}
+                    <MainPage cards={posts}/>
                 </main>
             </div>
         </div>
