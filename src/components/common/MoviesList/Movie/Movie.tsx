49a2281a69a5eab8030e6a791b0cde7814796364
AlexEmelianov7 <webstormfortms@gmail.com>
2022-11-23 15:03:13 +0300

    add slider for movie page

--- src/components/common/MoviesList/Movie/Movie.tsx
+++ src/components/common/MoviesList/Movie/Movie.tsx
@@ -6,20 +6,20 @@ import Rating, { RatingVariant } from './Rating/Rating';
 import Genres from './Genres/Genres';
 
 interface MovieProps {
-    movie: IMovie
+    movie: IMovie | undefined
     index?: number
 }
 
 const Movie: FC<MovieProps> = ({movie}) => {
     const navigate = useNavigate();
-    const handleMoviePageOpen = () => navigate(`/home/${movie.id}`)
+    const handleMoviePageOpen = () => navigate(`/home/${movie?.id}`)
 
     // style={{ backgroundImage: `url(${movie?.poster?.url})` }
     return (
         <div className={styles.movie}>
             <img src={movie?.poster?.url} onClick={handleMoviePageOpen} alt="img"/>
-            <h2 className={styles.movieTitle}>{movie.name}</h2>
-            <Rating variant={RatingVariant.kp} {...movie}/>
+            <h2 className={styles.movieTitle}>{movie?.name}</h2>
+            {!!movie?.rating && <Rating variant={RatingVariant.kp} {...movie}/>}
             <Genres {...movie} />
         </div>
     );
