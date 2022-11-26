a559aed10ae46c770886b07c1c4c9679a36003f7
Lada <bubnovichl@mail.ru>
2022-11-26 18:58:17 +0300

    add link to sign-in for navigation

--- src/components/common/MainWrapper/Header/UserInfo/NoNAuthUser/NoNAuthUser.tsx
+++ src/components/common/MainWrapper/Header/UserInfo/NoNAuthUser/NoNAuthUser.tsx
@@ -2,19 +2,22 @@ import React, { FC } from 'react';
 import nonUser from "../../../../../../assets/ArrowNoNAuth.svg"
 import User from "../../../../../../assets/user.svg"
 import styles from "./NoNAuthUser.module.scss"
+import { routes } from "../../../../../App/AppRoutesAuth/AppRouterAuth"
+import { Link } from 'react-router-dom';
 
 const NoNAuthUser: FC = () => {
-    return (
-        <div className={styles.nonUser}>
-            <div className={styles.nonUserAvatar}>
-                <img src={User} alt="User" />
-            </div>
-            <div className={styles.nonUserLeft}>
-                <p>Sign In</p>
-                <img src={nonUser} alt="Icon" />
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.nonUser}>
+			<div className={styles.nonUserAvatar}>
+				<img src={User} alt="User" />
+			</div>
+			<div className={styles.nonUserLeft}>
+				<Link to={routes.SIGN_IN} title="sign-in">
+					<p>Sign In</p> </Link>
+				<img src={nonUser} alt="Icon" />
+			</div>
+		</div>
+	);
 };
 
 export default NoNAuthUser;
