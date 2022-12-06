9858e917a5cc42531a2fda235e803e0587da4903
Lada <100614341+ladaln@users.noreply.github.com>
2022-12-06 17:24:32 +0300

    reset_password_some changes

--- src/components/common/Authorization/ResetPassword/ResetPassword.tsx
+++ src/components/common/Authorization/ResetPassword/ResetPassword.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 import { routes } from '../../../App/AppRoutesAuth/AppRouterAuth';
+
 import Input, { IInputData } from '../Input/Input';
 import { NotificationText } from '../NotificationBase/NotificationBase';
 
-import styles from './ResetPassword.module.scss';
 import NotificationBase from '../NotificationBase/NotificationBase';
+
+import styles from './ResetPassword.module.scss';
 
 const ResetPassword = () => {
 	const { register, handleSubmit } = useForm<IInputData>();
 	const navigate = useNavigate();
-	const [isDisable, setIsDisable] = useState(false); // для изменения вида модалки(обычная форма/ форма с отправленным email)
-	const [isDisableError, setIsDisableError] = useState(false);//
-	const [textErrorState, setTexErrorState] = useState();// если user не существует
-	
+	const [isDisable, setIsDisable] = useState(false);
+	const [isDisableError, setIsDisableError] = useState(false);
+	const [textErrorState, setTexErrorState] = useState<NotificationText>(NotificationText.reset_password);
+
 	const getErrorText = (responseText: string) => {
 		if (responseText === 'auth/user-not-found') {
 			setTexErrorState(NotificationText.email_not_found);
 		}
 	};
-	
-	const currentUserEmail = sessionStorage.getItem("userEmail"); 
+
+	const currentUserEmail = sessionStorage.getItem("userEmail");
 	const sendEmail = `На указанную эл.почту ${currentUserEmail} отправлена ссылка для восстановления пароля!`
-	
+
 	const onSubmit: SubmitHandler<IInputData> = (data) => {
 		const auth = getAuth();
-		const email = data.email; //получили введенный email пользователя, чтобы прокинуть его в текст Notification
+		const email = data.email;
 		sendPasswordResetEmail(auth, data.email)
 			.then(() => {
 				setIsDisable(true);
@@ -39,9 +41,13 @@ const ResetPassword = () => {
 			})
 			.then(() =>
 				sessionStorage.setItem("userEmail", email)
-			) 
+			)
+			.catch((error) => {
+				setIsDisableError(true);
+				getErrorText(error.code);
+			});
 	};
-	
+
 	return (
 		<>
 			{isDisable ? (
@@ -58,21 +64,23 @@ const ResetPassword = () => {
 					/>
 					<button className={styles.buttonReset}>Reset</button>
 				</form>
-			) : (
-				<form className={styles.formReset} onSubmit={handleSubmit(onSubmit)}>
-					<h2 className={styles.titleReset}>Восстановление пароля</h2>
-					{isDisableError && <NotificationBase message={textErrorState} />}
-					<Input
-						inputName="Адрес электронной почты"
-						inputType="email"
-						placeholder="Ваш адрес эл.почты"
-						keyData="email"
-						register={register}
-						required
-					/>
-					<button className={styles.buttonReset}>Восстановить</button>
-				</form>
-			)}
+			)
+				:
+				(
+					<form className={styles.formReset} onSubmit={handleSubmit(onSubmit)}>
+						<h2 className={styles.titleReset}>Восстановление пароля</h2>
+						{isDisableError && <NotificationBase message={textErrorState} />}
+						<Input
+							inputName="Адрес электронной почты"
+							inputType="email"
+							placeholder="Ваш адрес эл.почты"
+							keyData="email"
+							register={register}
+							required
+						/>
+						<button className={styles.buttonReset}>Восстановить</button>
+					</form>
+				)}
 		</>)
 };
 
