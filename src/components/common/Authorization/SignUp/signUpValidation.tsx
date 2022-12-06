d633157205d983f099a7b18f4378a595f2dfc6c8
Lada <100614341+ladaln@users.noreply.github.com>
2022-12-06 13:45:45 +0300

    Delete signUpValidation.tsx

--- src/components/common/Authorization/SignUp/signUpValidation.tsx
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-export const SignUpScheme = yup.object().shape(
-	{
-		email: yup.string().email("неверная почта").required("Почта обязательна"),
-		password: yup.string().min(6, "длина парол должна быть больше 6 символов").required("Пароль обязателен")
-	}
-)
